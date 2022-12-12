a15b66768b7a64d219694370ab0eb7c699f29e02
Emanoel Galdino <emanoel.galdino.cardoso@gmail.com>
2022-12-12 13:48:29 -0300

    Adicionado as pastas de estudo

---
+++ Python/For-and-while.py
@@ -0,0 +1,35 @@
+# Estudo de laço de Repetição
+
+#for (para)
+familia = ['Adeilton', 'Ana', 'Gabriel', 'Emanoel', 'Adeilton']
+posicao = ['Pai', 'Mãe', 'Irmão mais velho', 'Irmão Do meio', 'Imão mais novo']
+"""
+for x in familia:
+    print(x)
+
+print('Finish!')
+"""
+
+"""
+x = 'Emanoel'
+cont = 0
+
+while cont < len(familia):
+
+    if familia[cont] == x:
+        print('Encontrado.')
+        break
+    else:
+        print('Procurando...')
+
+    cont += 1
+"""
+cont = len(familia)
+for x in familia:
+    
+    
+
+
+
+
+
